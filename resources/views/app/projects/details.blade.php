80c608524c5509f0c2b0730b7a27524d404c670a
SamirM <maikap.samir@gmail.com>
2018-07-19 19:29:39 +0530

    Optimize web

--- resources/views/app/projects/details.blade.php
+++ resources/views/app/projects/details.blade.php
@@ -1,9 +1,12 @@
 @extends('layouts.app')
 @section('content')
     <main class="main-container">
-        <header class="header mb-0">
-            <div class="header-action center-h">
-                <nav class="nav">
+        <header class="header row p-0 m-0 mb-0" style="flex-direction: row">
+            <div class="col-lg-6">
+                <h3>Project Name</h3>
+            </div>
+            <div class="col-lg-6 center-h header-action " style="background-color: red">
+                <nav class="nav" style="background-color: green;float: right!important;">
                     <a class="nav-link" href="">Profile</a>
                     <a class="nav-link" href="">Awards</a>
                     <a class="nav-link active" href="">Performance</a>
