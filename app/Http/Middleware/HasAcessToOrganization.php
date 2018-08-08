271aed5e17ed312108b27fd49dfaabce359653d5
SamirM <maikap.samir@gmail.com>
2018-08-08 12:50:45 +0530

    Optimize web

--- app/Http/Middleware/HasAcessToOrganization.php
+++ app/Http/Middleware/HasAcessToOrganization.php
@@ -15,13 +15,24 @@ class HasAcessToOrganization
      */
     public function handle($request, Closure $next)
     {
-        if(session()->has('not_accessible')){
-            if(session()->get('not_accessible')=='invited')
-                return redirect(url('abort/invited'));
-            elseif(session()->get('not_accessible')=='subscription')
-                return redirect(url('abort/subscription'));
-            else
-                return redirect(url('abort/suspend'));
+        $organization_user=session()->get('organization_user');
+        $user_id=session()->get('user')->id;
+        $subscribed=session()->get('orgaization')->subscribed('main');
+        if(!$subscribed){
+            if(isAdmin()){
+                return redirect(url('organizations').'/'.$organization_user->id.'/view');
+            }
+            else{
+                return redirect(url('users').'/'.$user_id.'/profile');
+            }
+        }
+
+        if($organization_user->is_suspended==1){
+            return redirect(url('users').'/'.$user_id.'/profile');
+        }
+
+        if($organization_user->is_invited==1){
+            return redirect(url('users').'/'.$user_id.'/profile');
         }
 
         return $next($request);
