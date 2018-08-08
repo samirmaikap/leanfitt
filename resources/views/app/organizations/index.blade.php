c7940d71e342827a79f259732606291104d1e587
SamirM <maikap.samir@gmail.com>
2018-08-08 12:18:23 +0530

    Optimize web

--- resources/views/app/organizations/index.blade.php
+++ resources/views/app/organizations/index.blade.php
@@ -29,7 +29,7 @@
                             <td>{{ $organization->users_count }}</td>
                             <td>
                                 @if(isset($organization))
-                                    @if($organization->trial_ends_at && !date('Y-m-d',strtotime($organization->trial_ends_at))->isPast())
+                                    @if(!empty($organization->trial_ends_at) && !date('Y-m-d',strtotime($organization->trial_ends_at))->isPast())
                                         <span class="text-warning">On Trial</span>
                                     @else
                                         @if($organization->subscriptions[0]->ends_at && date('Y-m-d',strtotime($organization->subscriptions[0]->ends_at))->isPast())
